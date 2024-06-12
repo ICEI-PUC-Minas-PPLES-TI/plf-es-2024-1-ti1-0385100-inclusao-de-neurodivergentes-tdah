b93b10adcc5d392ea17eeef9942b15e002b41d17
Pazzutti <arthur.pazzutti0@gmail.com>
2024-06-12 11:53:55 -0300

    atualização pzt

--- codigo/pages/dashboard/dashboardjs.js
+++ codigo/pages/dashboard/dashboardjs.js
@@ -9,12 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function animateButton(button) {
         button.style.transition = 'background-color 0.3s, transform 0.3s';
-        button.style.backgroundColor = '#28a745'; // Change color to green
+        button.style.backgroundColor = '#fffff'; // Change color to green
         button.style.transform = 'scale(1.1)';
 
         // Revert back to original state after 300ms
         setTimeout(function() {
-            button.style.backgroundColor = '#007bff';
+            button.style.backgroundColor = '#fffff';
             button.style.transform = 'scale(1)';
         }, 300);
     }
